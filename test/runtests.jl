ce81476f769dec7edcd27fd5699758f76b5301a2
Alex Arslan <ararslan@comcast.net>
2019-01-04 00:11:44 -0800

    Allow coverage submission from Cirrus CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -429,6 +429,66 @@ end
                 end
             end
         end
+
+        # Test Cirrus CI submission process
+
+        # Set up Cirrus CI environment
+        withenv("CIRRUS_CI" => "true",
+                "CIRRUS_PR" => "t_pr",
+                "CIRRUS_BRANCH" => "t_branch",
+                "CIRRUS_CHANGE_IN_REPO" => "t_commit",
+                "CIRRUS_REPO_FULL_NAME" => "t_repo",
+                "CIRRUS_BUILD_ID" => "t_num") do
+            # default values
+            codecov_url = extract_codecov_url(()->Coverage.Codecov.submit(fcs, dry_run=true))
+            @test occursin("codecov.io", codecov_url)
+            @test occursin("service=cirrus", codecov_url)
+            @test occursin("branch=t_branch", codecov_url)
+            @test occursin("commit=t_commit", codecov_url)
+            @test occursin("pull_request=t_pr", codecov_url)
+            @test occursin("build=t_num", codecov_url)
+
+            # env var url override
+            withenv("CODECOV_URL" => "https://enterprise-codecov-1.com") do
+                codecov_url = extract_codecov_url(()->Coverage.Codecov.submit(fcs, dry_run=true))
+                @test occursin("enterprise-codecov-1.com", codecov_url)
+                @test occursin("service=cirrus", codecov_url)
+                @test occursin("branch=t_branch", codecov_url)
+                @test occursin("commit=t_commit", codecov_url)
+                @test occursin("pull_request=t_pr", codecov_url)
+                @test occursin("build=t_num", codecov_url)
+
+                # function argument url override
+                codecov_url = extract_codecov_url(()->Coverage.Codecov.submit(fcs, dry_run=true, codecov_url="https://enterprise-codecov-2.com"))
+                @test occursin("enterprise-codecov-2.com", codecov_url)
+                @test occursin("service=cirrus", codecov_url)
+                @test occursin("branch=t_branch", codecov_url)
+                @test occursin("commit=t_commit", codecov_url)
+                @test occursin("pull_request=t_pr", codecov_url)
+                @test occursin("build=t_num", codecov_url)
+
+                # env var token
+                withenv("CODECOV_TOKEN" => "token_name_1") do
+                    codecov_url = extract_codecov_url(()->Coverage.Codecov.submit(fcs, dry_run=true))
+                    @test occursin("enterprise-codecov-1.com", codecov_url)
+                    @test occursin("token=token_name_1", codecov_url)
+                    @test occursin("service=cirrus", codecov_url)
+                    @test occursin("branch=t_branch", codecov_url)
+                    @test occursin("commit=t_commit", codecov_url)
+                    @test occursin("pull_request=t_pr", codecov_url)
+                    @test occursin("build=t_num", codecov_url)
+
+                    # function argument token url override
+                    codecov_url = extract_codecov_url(()->Coverage.Codecov.submit(fcs, dry_run=true, token="token_name_2"))
+                    @test occursin("enterprise-codecov-1.com", codecov_url)
+                    @test occursin("service=cirrus", codecov_url)
+                    @test occursin("branch=t_branch", codecov_url)
+                    @test occursin("commit=t_commit", codecov_url)
+                    @test occursin("pull_request=t_pr", codecov_url)
+                    @test occursin("build=t_num", codecov_url)
+                end
+            end
+        end
     end
 end
 
